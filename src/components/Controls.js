f67f78945f5c6e7f68ea31bc5059e2bb26b12462
Nikita Korobkov <nikkorobk@gmail.com>
2018-06-15 20:21:22 +0300

    Nightmare controls appearance configuration

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import './Checkbox.css';
+
 
 class Controls extends Component {
 
@@ -6,10 +8,35 @@ class Controls extends Component {
         // should hold info about hotkeys, false start flag and signal time set field
 
         return (
-            <div className="Controls">
-                <p> Controls</p>
+            <div className="Sidebar">
+                <div className="Sidebar-container">
+                <div className="header">
+                    Brain Ring Button <br/>
+                    Alpha 0.0.1
+                </div>
+                    <br/>
+                    <br/>
+
+                <div className="Controls">
+                    <div className='timer-input-box'>
+                        <div className='timer-line'>Timer:   <input className="effect-16 timerInput" type="number" placeholder="10"/></div>
+                    </div>
+
+                    <div className='fs-input-box'>
+                        <span>False Start: <input className="regular-checkbox" type="checkbox"/></span>
+                    </div>
+
+
+                </div>
+
+                <div className="hint">
+                    {this.props.hint}
+                </div>
             </div>
-        );
+
+    </div>
+    )
+        ;
     }
 }
 
