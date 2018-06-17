dd34b9c91f9136ddb892dc0ca15181d6e402272a
Nikita Korobkov <nikkorobk@gmail.com>
2018-06-17 13:35:16 +0300

    Timer and sounds added

--- src/components/Engine.js
+++ src/components/Engine.js
@@ -4,11 +4,13 @@ import Player from "./Player";
 import Banner from "./Banner";
 import Mousetrap from 'mousetrap';
 import {GameState, PlayerState, GameStateHint} from "../constants/States";
-
+import {timerStart, buttonPressed, timerEnd} from "./SoundController";
 
 class Engine extends Component {
     BUTTONS_BLOCKED_GAME_STATES = [GameState.pressed, GameState.operational];
     BUTTONS_BLOCKED_PLAYER_STATES = [PlayerState.disq, PlayerState.pressed];
+    TIMER_BLOCKED_GAME_STATES = [GameState.pressed, GameState.operational, GameState.timer];
+
     DEFAULT_PLAYERS_STATE = {1: PlayerState.ready, 2: PlayerState.ready, 3: PlayerState.ready};
 
     constructor(props) {
@@ -23,6 +25,7 @@ class Engine extends Component {
         this.onBannerClick = this.onBannerClick.bind(this);
         this.onFSClick = this.onFSClick.bind(this);
         this.onTimerChange = this.onTimerChange.bind(this);
+        this.onStartTimerClick = this.onStartTimerClick.bind(this);
         this.newGame = this.newGame.bind(this);
         this.startOperational = this.startOperational.bind(this);
     }
@@ -30,30 +33,44 @@ class Engine extends Component {
     componentDidMount() {
         Mousetrap.bind(['space'], this.newGame);
         Mousetrap.bind(['backspace'], this.startOperational);
+        Mousetrap.bind(['t'], this.onStartTimerClick);
     }
 
     componentWillUnmount() {
         Mousetrap.unbind(['space'], this.newGame);
         Mousetrap.unbind(['backspace'], this.startOperational);
+        Mousetrap.unbind(['t'], this.onStartTimerClick);
     }
 
     startOperational(){
         this.setState({gameState: GameState.operational, playersState: this.DEFAULT_PLAYERS_STATE})
+        this.stopTimer();
     }
 
+    onStartTimerClick(){
+        if(this.TIMER_BLOCKED_GAME_STATES.includes(this.props.gameState)){return}
+        this.startTimer(this.state.qTime)
+    }
+
+    startTimer(val){
+        this.setState({gameState:GameState.timer});
+        this.timerID = setTimeout(() => this.timerDone(), +val * 1000 );
+        timerStart.play();
+    }
+
+    timerDone(){
+        timerEnd.play();
+        this.reset()
+    }
+
+    stopTimer(){
+        if(this.timerID) {
+            clearTimeout(this.timerID);
+        }
+    }
     onBannerClick(e) {
         if (this.BUTTONS_BLOCKED_GAME_STATES.includes(this.state.gameState )) return;
         this.playerClicked(e.nativeEvent.which);
-    }
-
-    playerClicked(index) {
-        if (this.BUTTONS_BLOCKED_PLAYER_STATES.includes(this.state.playersState[index])) return;
-        if (this.isFalseStart()) {
-            this.disqualifyPlayer(index)
-        } else {
-            this.activatePlayer(index);
-            this.setState({gameState: GameState.pressed})
-        }
     }
 
     onFSClick(){
@@ -62,6 +79,28 @@ class Engine extends Component {
 
     onTimerChange(val){
         this.setState((s) => ({qTime : +val}))
+    }
+
+    newGame() {
+        this.reset();
+        this.stopTimer();
+    }
+
+    reset(){
+        let ps = Object.assign({}, this.DEFAULT_PLAYERS_STATE);
+        this.setState({gameState: GameState.ready, playersState: ps })
+    }
+
+    playerClicked(index) {
+        if (this.BUTTONS_BLOCKED_PLAYER_STATES.includes(this.state.playersState[index])) return;
+        if (this.isFalseStart()) {
+            this.disqualifyPlayer(index)
+        } else {
+            buttonPressed.play();
+            this.activatePlayer(index);
+            this.stopTimer();
+            this.setState({gameState: GameState.pressed})
+        }
     }
 
     isFalseStart() {
@@ -85,13 +124,6 @@ class Engine extends Component {
             };
         })
     }
-
-    newGame() {
-        console.log("ng");
-        let ps = Object.assign({}, this.DEFAULT_PLAYERS_STATE);
-        this.setState({gameState: GameState.ready, playersState: ps })
-    }
-
 
     render() {
         return (
---
+++ src/components/SoundController.js
@@ -0,0 +1,8 @@
+import timerStartSound from '../res/timerStart.wav';
+import timerEndSound from '../res/timerEnd.wav';
+import buttonPressedSound from '../res/buttonPressed.wav';
+
+
+export const timerStart = new Audio(timerStartSound);
+export const timerEnd = new Audio(timerEndSound);
+export const buttonPressed = new Audio(buttonPressedSound);
