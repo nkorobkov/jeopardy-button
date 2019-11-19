27d27689c18e15034fb8e8f12c23e3afd67dd6e4
Nikita Korobkov <nikkorobk@gmail.com>
2019-11-19 15:16:52 -0500

    Updates

---
+++ precache-manifest.377b56aa77d2cb8820a13ce543c161ec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "395433749e91983fc7754cc187436001",
+    "url": "/jeopardy-button/index.html"
+  },
+  {
+    "revision": "8f3c895839dfb0526041",
+    "url": "/jeopardy-button/static/css/main.f4b9d68f.chunk.css"
+  },
+  {
+    "revision": "8014f2ab3a78411111e8",
+    "url": "/jeopardy-button/static/js/2.9a693294.chunk.js"
+  },
+  {
+    "revision": "8f3c895839dfb0526041",
+    "url": "/jeopardy-button/static/js/main.a98db08e.chunk.js"
+  },
+  {
+    "revision": "7667384eaac0f00d2468",
+    "url": "/jeopardy-button/static/js/runtime~main.1db371de.js"
+  },
+  {
+    "revision": "b1d84b3d2f4660372bec196d71183d7f",
+    "url": "/jeopardy-button/static/media/buttonPressed.b1d84b3d.wav"
+  },
+  {
+    "revision": "1f1d48344499111bd78f9b10d9f43c4c",
+    "url": "/jeopardy-button/static/media/timerEnd.1f1d4834.wav"
+  },
+  {
+    "revision": "fcf250c6af2de24d8b84c2e37a748d57",
+    "url": "/jeopardy-button/static/media/timerStart.fcf250c6.wav"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,39 @@
-"use strict";var precacheConfig=[["/brain-ring-button/index.html","8a721ef4b655ca86165d06bf831eda97"],["/brain-ring-button/static/css/main.8fb03f0c.css","e530eb23e787b075237a4e825b96e756"],["/brain-ring-button/static/js/main.796b8028.js","3f156d3310886528048661d77b9adaf3"],["/brain-ring-button/static/media/buttonPressed.b1d84b3d.wav","b1d84b3d2f4660372bec196d71183d7f"],["/brain-ring-button/static/media/timerEnd.1f1d4834.wav","1f1d48344499111bd78f9b10d9f43c4c"],["/brain-ring-button/static/media/timerStart.fcf250c6.wav","fcf250c6af2de24d8b84c2e37a748d57"]],cacheName="sw-precache-v3-sw-precache-webpack-plugin-"+(self.registration?self.registration.scope:""),ignoreUrlParametersMatching=[/^utm_/],addDirectoryIndex=function(e,t){var n=new URL(e);return"/"===n.pathname.slice(-1)&&(n.pathname+=t),n.toString()},cleanResponse=function(t){return t.redirected?("body"in t?Promise.resolve(t.body):t.blob()).then(function(e){return new Response(e,{headers:t.headers,status:t.status,statusText:t.statusText})}):Promise.resolve(t)},createCacheKey=function(e,t,n,r){var a=new URL(e);return r&&a.pathname.match(r)||(a.search+=(a.search?"&":"")+encodeURIComponent(t)+"="+encodeURIComponent(n)),a.toString()},isPathWhitelisted=function(e,t){if(0===e.length)return!0;var n=new URL(t).pathname;return e.some(function(e){return n.match(e)})},stripIgnoredUrlParameters=function(e,n){var t=new URL(e);return t.hash="",t.search=t.search.slice(1).split("&").map(function(e){return e.split("=")}).filter(function(t){return n.every(function(e){return!e.test(t[0])})}).map(function(e){return e.join("=")}).join("&"),t.toString()},hashParamName="_sw-precache",urlsToCacheKeys=new Map(precacheConfig.map(function(e){var t=e[0],n=e[1],r=new URL(t,self.location),a=createCacheKey(r,hashParamName,n,/\.\w{8}\./);return[r.toString(),a]}));function setOfCachedUrls(e){return e.keys().then(function(e){return e.map(function(e){return e.url})}).then(function(e){return new Set(e)})}self.addEventListener("install",function(e){e.waitUntil(caches.open(cacheName).then(function(r){return setOfCachedUrls(r).then(function(n){return Promise.all(Array.from(urlsToCacheKeys.values()).map(function(t){if(!n.has(t)){var e=new Request(t,{credentials:"same-origin"});return fetch(e).then(function(e){if(!e.ok)throw new Error("Request for "+t+" returned a response with status "+e.status);return cleanResponse(e).then(function(e){return r.put(t,e)})})}}))})}).then(function(){return self.skipWaiting()}))}),self.addEventListener("activate",function(e){var n=new Set(urlsToCacheKeys.values());e.waitUntil(caches.open(cacheName).then(function(t){return t.keys().then(function(e){return Promise.all(e.map(function(e){if(!n.has(e.url))return t.delete(e)}))})}).then(function(){return self.clients.claim()}))}),self.addEventListener("fetch",function(t){if("GET"===t.request.method){var e,n=stripIgnoredUrlParameters(t.request.url,ignoreUrlParametersMatching),r="index.html";(e=urlsToCacheKeys.has(n))||(n=addDirectoryIndex(n,r),e=urlsToCacheKeys.has(n));var a="/brain-ring-button/index.html";!e&&"navigate"===t.request.mode&&isPathWhitelisted(["^(?!\\/__).*"],t.request.url)&&(n=new URL(a,self.location).toString(),e=urlsToCacheKeys.has(n)),e&&t.respondWith(caches.open(cacheName).then(function(e){return e.match(urlsToCacheKeys.get(n)).then(function(e){if(e)return e;throw Error("The cached response that was expected is missing.")})}).catch(function(e){return console.warn('Couldn\'t serve response for "%s" from cache: %O',t.request.url,e),fetch(t.request)}))}});
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/jeopardy-button/precache-manifest.377b56aa77d2cb8820a13ce543c161ec.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/jeopardy-button/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
